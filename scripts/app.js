937bbeb14a577e985b418335cc8d7a5dce8a5f80
Mateusz Wójcik <mateusz-wojcik@hotmail.com>
2020-05-23 23:45:15 +0200

    comments added, sorting asc and desc, remove sorting class(dom)

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 data.map(data => {
+  // print data from array into dom
   const row = document.createElement("tr");
   const root = document.getElementById("root");
   root.appendChild(row);
@@ -12,26 +13,44 @@ data.map(data => {
 var table = document.querySelector(".table"),
   ths = table.querySelectorAll("thead th"),
   trs = table.querySelectorAll("tbody tr"),
-  df = document.createDocumentFragment();
-let tHead = document.querySelectorAll("th");
-let rows = document.querySelectorAll("tbody tr");
+  df = document.createDocumentFragment(),
+  tHead = document.querySelectorAll("th"),
+  rows = document.querySelectorAll("tbody tr");
+
+// removing class standing for signal to sort
+const clearClass = nodeList =>
+  nodeList.forEach(nodeElement => {
+    nodeElement.className = "";
+  });
 
 const sortBy = e => {
+  // convert node lists into array for iterating methods
   tHead = Array.from(tHead);
   rows = Array.from(rows);
-  let i = e.target.cellIndex;
+  let i = e.target.cellIndex,
+    order =
+      e.target.className === "" || e.target.className === "desc"
+        ? "asc"
+        : "desc";
+
+  clearClass(tHead);
+
   rows.sort((a, b) => {
     let tdA = a.children[i].textContent,
       tdB = b.children[i].textContent;
+    // sorting
     if (tdA < tdB) {
-      return -1;
+      return order === "asc" ? -1 : 1;
     } else if (tdA > tdB) {
-      return 1;
+      return order === "asc" ? 1 : -1;
     } else {
       return 0;
     }
   });
+  // set class
+  e.target.className = "order";
 
+  // "render" sorted list
   rows.forEach(function(tr) {
     df.appendChild(tr);
   });
